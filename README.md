17ad43ed0aba7961beb708bfa20d146be093be21
Ning Zhong <nzhong@ebsuite.com>
2015-11-06 21:41:13 -0800

    Second Commit: add servlet handler, so now we can handle static contents in resources, and servlets. What's missing is REST/JSON handling.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Simple server is a starting point for a micro-service java web server. It is
 
 * based on undertow
 * should support static content, servlet, and REST APIs.
-* should be really easy to host a AngularJS app, or a RestAPI server.
+* should be really easy to host an AngularJS app, a ReactJS app, or a RestAPI server.
 
 
 <br />
@@ -20,4 +20,7 @@ How to run?
 ---------------------------------------------------------
 * ```mvn clean package```
 * ```mvn exec:java```
-* Point browser to http://localhost:6060/api and http://localhost:6060/static
+* Point browser to:
+* http://localhost:6060/api   should return api
+* http://localhost:6060/static  should return static html/css/js
+* http://localhost:6060/serv/hello  is handled by servlet
--- src/main/java/org/simpserv/test/Test.java
+++ src/main/java/org/simpserv/test/Test.java
@@ -6,25 +6,31 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.server.handlers.resource.ResourceHandler;
 import io.undertow.server.handlers.resource.ResourceManager;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
 import io.undertow.util.Headers;
+import org.simpserv.servlets.EchoServlet;
+import org.simpserv.servlets.HelloServlet;
 
 import static io.undertow.Handlers.path;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Undertow server = Undertow.builder()
             .addHttpListener(6060, "localhost")
             .setHandler(path()
                 .addPrefixPath( "/static", createStaticResourceHandler() )
                 .addPrefixPath( "/api",    new InternalRestHandler() )
+                .addPrefixPath( "/serv",   createServletHandler() )
             ).build();
         server.start();
     }
 
     private static HttpHandler createStaticResourceHandler() {
         final ResourceManager staticResources = new ClassPathResourceManager(
-                (new Test()).getClass().getClassLoader(),"static");
+                Test.class.getClassLoader(),"static");
         final ResourceHandler resourceHandler = new ResourceHandler(staticResources);
         resourceHandler.setWelcomeFiles("index.html");
         return resourceHandler;
@@ -36,5 +42,20 @@ public class Test {
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
             exchange.getResponseSender().send("Hello World U");
         }
+    }
+
+    private static HttpHandler createServletHandler() throws Exception {
+        DeploymentInfo di = Servlets.deployment()
+                .setClassLoader(Test.class.getClassLoader())
+                .setContextPath("/serv")
+                .setDeploymentName("My Servlets")
+                .addServlets(
+                    Servlets.servlet("helloServlet", HelloServlet.class).addMapping("/hello"),
+                    Servlets.servlet("echoServlet", EchoServlet.class).addMapping("/echo")
+                );
+        DeploymentManager manager = Servlets.defaultContainer().addDeployment(di);
+        manager.deploy();
+        HttpHandler servletHandler = manager.start();
+        return servletHandler;
     }
 }
